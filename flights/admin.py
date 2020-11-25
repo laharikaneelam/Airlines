07d88c4f3bcde8beb8166e24becaf3ed4b0ed612
laharika <luckyneelam97@gmail.com>
2020-11-25 12:25:53 +0530

    added files

---
+++ flights/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Flight,Airport,Passenger
+# Register your models here.
+class flightadmin(admin.ModelAdmin):
+    list_display=("id", "origin","destination","duration")
+
+class passengeradmin(admin.ModelAdmin):
+    filter_horizontal=("flights",)
+
+
+admin.site.register(Flight,flightadmin)
+admin.site.register(Airport)
+admin.site.register(Passenger,passengeradmin)
